bda0d277194af340be05a351b689e27da8f43a34
Márcio S. Manske <mmanske@gmail.com>
2017-02-09 17:54:42 -0200

    Error message correction

--- src/app/dto/SchoolSearch.ts
+++ src/app/dto/SchoolSearch.ts
@@ -4,11 +4,11 @@
 
 export class SchoolSearch {
 
-  constructor(public name: string, 
-              public city: string, 
-              public state: string, 
-              public country: string, 
-              public language: string, 
-              public status: string) {}
+  constructor(public name: string = null,
+              public city: string = null,
+              public state: string = null,
+              public country: string = null,
+              public language: string = "ALL",
+              public status: string = "ALL") {}
 
 }
