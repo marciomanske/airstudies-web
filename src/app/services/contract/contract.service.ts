558ac3cb9f036eaa49f114a1e844a75d8f5b3401
Wendel Sagrilo <wendelsagrilo@gmail.com>
2017-02-14 19:36:14 -0200

    Property register

---
+++ src/app/services/contract/contract.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {BaseHttpService} from "../base/base-http.service";
+import {  Http }    from '@angular/http';
+import {ConfigService} from "../../config/config.service";
+import {Contract} from "../../dto/Contract";
+
+@Injectable()
+export class ContractService extends BaseHttpService {
+
+  constructor(protected http: Http, protected config: ConfigService) { 
+  super (http, "contract", config);
+  }
+}
