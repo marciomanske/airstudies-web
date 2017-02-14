007cc42dcccaa45f9fe6c8db5212b607b3e6719b
Wendel Sagrilo <wendelsagrilo@gmail.com>
2017-02-14 19:36:32 -0200

    Property register

---
+++ src/app/dto/ContractSearch.ts
@@ -0,0 +1,11 @@
+export class ContractSearch {
+    constructor(
+    
+    public contract: string = null,
+    public school: string = null,
+    public city: string = null,
+    public country: string = null,
+    public state: string = null,
+    public status: string
+    
+    ){}}
---
+++ src/app/dto/RoomType.ts
@@ -0,0 +1,6 @@
+export class RoomType {
+    constructor (public label:string = null, public identifier: string = null,
+    public amount:number = null
+        ){}
+
+}
